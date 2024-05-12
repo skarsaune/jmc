3e7fffdb9f58b99580d5dd8155363787d770593c
Martin Skarsaune <martin@skarsaune.net>
2024-05-12 20:53:19 +0200

    REFACTOR: Actually added the new interface to git ....

---
+++ application/org.openjdk.jmc.jolokia/src/main/java/org/openjdk/jmc/jolokia/JolokiaDiscoverySettings.java
@@ -0,0 +1,10 @@
+package org.openjdk.jmc.jolokia;
+
+import org.jolokia.server.core.service.api.JolokiaContext;
+
+public interface JolokiaDiscoverySettings {
+	boolean shouldRunDiscovery();
+
+	JolokiaContext getJolokiaContext();
+
+}
